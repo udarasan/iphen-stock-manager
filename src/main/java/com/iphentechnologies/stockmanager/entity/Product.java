c1093bffb42dea1c20d1ee3623d3a050228d69dc
udarasan <udarassanjeewa@gmail.com>
2021-11-06 20:34:35 +0530

    update to heroku access

--- src/main/java/com/iphentechnologies/stockmanager/entity/Product.java
+++ src/main/java/com/iphentechnologies/stockmanager/entity/Product.java
@@ -3,14 +3,12 @@ package com.iphentechnologies.stockmanager.entity;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "products")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/iphentechnologies/stockmanager/entity/User.java
+++ src/main/java/com/iphentechnologies/stockmanager/entity/User.java
@@ -4,15 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
